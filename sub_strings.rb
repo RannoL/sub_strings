7fd6d0feada12828c8343b95ed415b748b46d853
RannoLauri <rannolauri@gmail.com>
2020-05-27 14:37:39 +0300

    Can process input strings with multiple words.

--- sub_strings.rb
+++ sub_strings.rb
@@ -2,12 +2,16 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(string, dictionary)
   result_list = Hash.new 0 #Sets default for any key to 0. {} sets nil. 1 + nil = error
-  dictionary.each do |word|
+  string_arr = string.downcase.gsub(/\p{Punct}/, "").split
+
+  string_arr.each do |string|
+    dictionary.each do |word|
     if string[word]
       result_list[word] += 1
     end
-  end
-  puts result_list
+  end 
+end
+  puts Hash[result_list.sort]
 end
 
-substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
